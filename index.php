be5deb3d7b866bc91afc540c8feca5eb60083cf3
arshdeep singh <arshpanesar007@gmail.com>
2025-06-04 08:18:51 +0530

    add header,basic structure

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<?php
+    include('./client/commonFiles.php');
+?>
+   
+    <title>Ask Bridge</title>
+</head>
+<body>
+    <?php 
+
+    include('./client/header.php');
+
+?>
+</body>
+</html>
